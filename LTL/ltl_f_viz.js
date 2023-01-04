5af09bb03ab37b0d5cee830a0acfedd041f00088
Tim Nelson <tn@cs.brown.edu>
2023-01-04 13:50:51 -0500

    working on LTLf

--- LTL/ltl_f_viz.js
+++ LTL/ltl_f_viz.js
@@ -10,11 +10,18 @@
     * accessibility concerns (e.g. zoom in, or open web dev console)
     * there's no way to change the size of the SVG subwindow to make more room for the editor
     * there's no docs links for the library functions
+
+    * It's really, really easy to mix up here:
+      "the atom State0" and "the singleton set State0"
 */
 
+const stage = new Stage()
 
   // ? how to render transition arrows between states? 
   // ? Call to Function() blocked by CSP: reload Sterling page
+
+const formulas = [...Binary.atoms(), ...Unary.atoms(), ...Atom.atoms()]
+const traces = [...Trace.atoms()]
 
 ////////////////////
 // Formulas
@@ -43,12 +50,11 @@ function formula2String(f) {
   // Formulas.atoms() will be empty, since
   // an atom appears in the _lowest-level_ sig it belongs to.
   // Don't confuse "Atom" and atom()
-  const formulas = [...Binary.atoms(), ...Unary.atoms(), ...Atom.atoms()]
   const formulaStrings = formulas.map(formula2String)
   
   // TODO: this should be more sophisticated
   //   10 isn't enough for atoms
-  const CHARWIDTH = 6
+  const CHARWIDTH = 7
   const horzFormulaSize = Math.max(...formulaStrings.map(s => s.length)) * CHARWIDTH
   const formulaGridConfig = {
     grid_location :{
@@ -60,30 +66,49 @@ function formula2String(f) {
         y_size:40
     },
     grid_dimensions:{
-        y_size:formulas.length,
+        y_size:formulas.length+1,
         x_size:3
     }
   }
-  
+
+  function traceStatePair2String(pair) {      
+      t = pair.atoms()[0]
+      s = pair.atoms()[1]
+      traceStates = trace2States(t)
+      
+      
+      // EXAMPLE: debugging without console to help determine types
+      //   since console shows proxies, which is very confusing
+    //   stage.add(new TextBox(`${Object.keys(s)}`,{x:100, y:100},'black',16)); 
+    //   stage.add(new TextBox(`${Object.keys(t)}`,{x:100, y:150},'black',16)); 
+    //   stage.add(new TextBox(`${traceStates}`,{x:100, y:200},'black',16)); 
+    //   stage.add(new TextBox(`${traceStates.map(x => Object.keys(x))}`,{x:100, y:250},'black',16));     
+      
+      return `${traces.indexOf(t)}@${traceStates.indexOf(s)}`
+  }
+    
   const formulaGrid = new Grid(formulaGridConfig)
+  formulaGrid.add({x:0, y:0}, new TextBox(`Fmla ID`,{x:0,y:0},'black',16))
+  formulaGrid.add({x:1, y:0}, new TextBox(`Syntax`,{x:0,y:0},'black',16))
+  formulaGrid.add({x:2, y:0}, new TextBox(`True in...`,{x:0,y:0},'black',16))
+
   formulaStrings.forEach( (fstr,i) => {                 
           const label = `${fstr}`
-          formulaGrid.add({x:0, y:i}, new TextBox(`${i}`,{x:0,y:0},'black',16)); 
-          formulaGrid.add({x:1, y:i}, new TextBox(label,{x:0,y:0},'black',16)); 
+          formulaGrid.add({x:0, y:i+1}, new TextBox(`${i}`,{x:0,y:0},'black',16)); 
+          formulaGrid.add({x:1, y:i+1}, new TextBox(label,{x:0,y:0},'black',16)); 
   })
   
   formulas.forEach( (f, i) => {
       const whereTrue = Semantics.table.join(f).tuples()
-      const label = `true in: [${whereTrue}]`
-      formulaGrid.add({x:2, y:i}, new TextBox(label,{x:0,y:0},'black',16)); 
+      const label = `[${whereTrue.map(traceStatePair2String)}]`
+      formulaGrid.add({x:2, y:i+1}, new TextBox(label,{x:0,y:0},'black',16)); 
 
   })
   
   ////////////////////
   // TRACES
   ////////////////////
-  const traces = [...Trace.atoms()]
-  const maxTraceLength = Math.max(...traces.map(t => t.next.tuples().length))
+  const maxTraceLength = Math.max(...traces.map(t => t.next.tuples().length + 1))
   
   const traceGridConfig = {
     grid_location :{
@@ -96,7 +121,7 @@ function formula2String(f) {
         y_size:70
     },
     grid_dimensions:{
-        y_size:traces.length,
+        y_size:traces.length+1,
         x_size:maxTraceLength+2
     }
   }
@@ -111,9 +136,13 @@ function formula2String(f) {
   // Produce a list of state atoms in the trace, ordered
   function trace2States(t) {
         states = []
-        var todo = t.first.tuples()[0]
+        var todo = t.first
         while(!todo.empty()) {
-            states.push(todo)
+            // NOTE WELL:
+            // todo is an expression
+            // todo.tuples() is an array of AlloyTuple
+            // todo.tuples()[0].atoms() is an array of AlloyAtom
+            states.push(todo.tuples()[0].atoms()[0])
             todo = todo.join(t.next)
         }            
         return states
@@ -137,24 +166,30 @@ function formula2String(f) {
   const traceGrid = new Grid(traceGridConfig)
   traceGrid.hide_grid_lines()
 
+  for(i = 0;i<maxTraceLength;i++) {
+    traceGrid.add({x:i+1, y:0}, new TextBox(`t=${i}`,{x:0,y:0},'black',16))
+  }
+
   traces.forEach( (t,traceIdx) => 
       {
-          traceGrid.add({x:0, y:traceIdx}, new TextBox(`${t}`,{x:0,y:0},'black',16)); 
+          traceGrid.add({x:0, y:traceIdx+1}, new TextBox(`Tr${traceIdx}`,{x:0,y:0},'black',16)); 
           const stateShapes = trace2ShapeArray(t)
           stateShapes.forEach( (stateShape, stateIdx) => {
-            traceGrid.add({x:stateIdx+1, y:traceIdx}, stateShape); 
+            traceGrid.add({x:stateIdx+1, y:traceIdx+1}, stateShape); 
           })
           
       }
   )
   
-  const stage = new Stage()
+
   stage.add(formulaGrid)
   stage.add(traceGrid)
   stage.render(svg, document)
   
   
-  
+  // Kinda want a "what sterlingType is this" field
+  //  if it has .atoms() and .tuples(), it is an AlloyTuple
+  //  if it has .tuples() and .id(), it is an AlloyAtom
   
   
   
