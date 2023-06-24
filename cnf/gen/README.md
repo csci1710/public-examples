60762d5f44f27f10aa50a1c8291b9fee16e36e9c
Tim Nelson <tn@cs.brown.edu>
2023-06-24 10:44:35 -0400

    working on CNF

--- cnf/gen/README.md
+++ cnf/gen/README.md
@@ -4,9 +4,40 @@
 
 I generated them via the [`cnfgen`](https://massimolauria.net/cnfgen/) Python library. The `cnfgen` tool can produce DIMACS standard CNF files for a variety of standard problems (including many more than I generated here as an example). 
 
+Normally, by the time these reach DIMACS, all variables are natural numbers; the parameters for each are lost. E.g., we can know that variable `5` corresponds to (say) a certain pigeon nesting in a certain pigeonhole, but not _which ones_. Fortunately, `cnfgen` has a `--varnames` argument which we can use to embed the variable mapping into comments in the DIMACS output. 
+
+### CNFGen Shapes
+
+The documentation and output don't detail the exact variable mapping, so I've extracted the below from the [source code](https://github.com/MassimoLauria/cnfgen) with minor editing. In some cases, there was a comment giving the mapping, in others I had to extrapolate from code.
+
+#### [Pigeonhole](https://github.com/MassimoLauria/cnfgen/blob/master/cnfgen/families/pigeonhole.py)
+
+```
+The formula is encoded with variables `p_{i,j}` for `i` in `[m]` and `j` in `[n]` where the intended meaning is that `p_{i,j}` is `True` when pigeon `i` flies into hole `j`.
+```
+
+#### [K-coloring of graphs](https://github.com/MassimoLauria/cnfgen/blob/master/cnfgen/families/coloring.py)
+
+The variables are of the form `x_{v,c}` where `v` is a vertex and `c` a color. 
+
+#### [Partitioning of elements](https://github.com/massimolauria/cnfgen/blob/master/cnfgen/families/counting.py)
+
+The variables are of the form `x_{subset}` where `subset` is a list of element IDs. That is, if a variable is true, then the corresponding subset is part of the partition.
+
 ## Which specific examples are here?
 
-This repo includes the original CNF files (`.cnf`) along with S-expression versions (`_t.cnf`). I've included the rough script used to convert (`translate.py`). 
+This repo includes the original CNF files (`.cnf`) along with S-expression versions (`.scnf`) that use the provided variable mappings to produce something more semantic. The form of an `.scnf` is:
+
+```
+((p cnf <num vars> <num clauses>)
+ clauses
+ ...)
+```
+where each clause is a list of literals of the form: `(not (var <variable parameters>))` or `(var <variable parameters>)`. Parameters are extracted from the DIMACS output with `--varnames` on.
+
+E.g., instead of a clause that looks like `(1 2 -3)`, if the `cnf` file contains a variable mapping where `1`, `2`, and `3` mean `p_{1,2}`, `p_{3,4}` and `p_{5,6}` respectively, the clause will be `((var 1 2) (var 3 4) (var 5 6))`  
+
+I've included the rough script used to convert (`translate.py`). 
 
 I generated 6 CNF files:
 
--- cnf/gen/translate.py
+++ cnf/gen/translate.py
@@ -1,30 +1,58 @@
 # Translate (simplified) DIMACS CNF input format to an s-expression
-# E.g., run cnfgen --output php3.cnf php 3 and then run this script on php3.cnf
-from dataclasses import dataclass
+# E.g., run cnfgen --varnames --output php3.cnf php 3 and then run this script on php3.cnf
+# IMPORTANT: you must make sure to include `--varnames`, or there will be 
+#   no variable mapping present in the input, and this script will be unable
+#   to extract semantic information about the variables.
 
+from dataclasses import dataclass
+import re
 
 @dataclass
 class CNF:
     num_vars: int
     num_clauses: int
+    variables: dict[int, str]
     clauses: list[list[int]]
 
-    def __str__(self):        
-        return f'((p cnf {self.num_vars} {self.num_clauses}) {"".join(["".join([str(l) for l in c]) for c in self.clauses])})'
+    def pretty_meaning(self, s) -> str:
+        '''Convert a varname like "p_{1,2}" into s-expression form'''
+        matched = re.findall(r"\d+", s)
+        return f'(var {" ".join(matched)})'
+
+    def pretty_literal(self, l) -> str:
+        '''Convert a DIMACS literal like "-1" into s-expression form'''
+        v = abs(int(l))
+        if v not in self.variables.keys(): raise ValueError(f'unknown var: {l}')
+        if(l < 0): return f'(not {self.pretty_meaning(self.variables[v])})'
+        else: return f'{self.pretty_meaning(self.variables[v])}'
+
+    def __str__(self):
+        clauses_str = " "+"\n ".join(["("+" ".join([self.pretty_literal(l) for l in c])+")" for c in self.clauses])
+        return f'((p cnf {self.num_vars} {self.num_clauses})\n{clauses_str})'
+
+def process_comment(split_line, variables):
+    if len(split_line) < 2: return
+    if split_line[1] != 'varname': return
+    vid = int(split_line[2])
+    vexpand = split_line[3] 
+    if vid in variables.keys(): raise ValueError(f'duplicate variable meaning: {vid}')
+    variables[vid] = vexpand
 
 def translate(filename) -> CNF:
     cnf_file = open(filename, 'r')
     lines: list[str] = cnf_file.readlines()        
     cnf_file.close()
 
-    clauses = []
+    clauses: list[list[int]] = []
+    variables: dict[int, str] = dict()
     num_vars = None
     num_clauses = None
 
     for line in lines:
         split_line: list[str] = line.split()        
         if len(split_line) < 1: pass   # resilient to blank lines
-        elif split_line[0] == 'c': pass  # a comment
+        elif split_line[0] == 'c': # a comment
+            process_comment(split_line, variables)
         elif split_line[0] == 'p':       # problem declaration
             if len(split_line) != 4: raise ValueError(f'p line had unexpected form: {split_line}')
             if split_line[1] != 'cnf': raise ValueError(f'p line had unexpected form: {split_line}')
@@ -35,19 +63,34 @@ def translate(filename) -> CNF:
         elif split_line[-1] != '0': raise ValueError(f'clause should be 0-terminated: {split_line}')
         else: 
             split_line = split_line[:-1]
-            clauses.append('('+' '.join(split_line)+")")
+            clauses.append([int(l) for l in split_line])
     if num_vars == None or num_clauses == None:
-        return CNF(0, 0, [])        
-    return CNF(num_vars, num_clauses, clauses)
+        return CNF(0, 0, dict(), [])
+    return CNF(num_vars, num_clauses, variables, clauses)
 
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) != 2: # usual first arg is the name of the script
-        print("Usage: python3 translate.py <CNF filename>")
-        exit()
-    result = translate(sys.argv[1])
-    out_filename = sys.argv[1].split('.')[0] + '_t.cnf'
+def convert_file(filename):
+    result = translate(filename)
+    out_filename = filename.split('.')[0] + '.scnf'
     out_file = open(out_filename, 'w')
     out_file.write(str(result))
     out_file.close()
+    
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 2: 
+        print('''Usage: python3 translate.py [CNF filename]
+        Provide a filename to convert only that filename.
+        Provide no filename to convert all .cnf files in the directory.
+        
+        For the moment, this utility just writes the translation to 
+        a file with the same name, but with the ".scnf" extension.''')
+        exit()
+    if len(sys.argv) == 2:
+        convert_file(sys.argv[1])
+    else:
+        import os
+        for file in os.listdir("."):
+            if file.endswith(".cnf"):
+                print(f'Processing: {file}')
+                convert_file(file)
     
